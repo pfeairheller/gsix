d21704ab6ddfddf3aebdb820768ff6781c535700
pfeairheller <pfeairheller@gmail.com>
2013-07-20 17:08:52 -0400

    More Response methods implemented.  Got stuck on whether to supply the "Send" method taking an interface and use reflection, or to provide "SendStruct", "Send" (for strings) "SendBuffer".  I don't like the thought of using reflection at the request level, so I'm leaning towards the separate methods.

--- response.go
+++ response.go
@@ -6,11 +6,13 @@ import (
 
 type GResponse struct {
 	raw http.ResponseWriter
+	charset string
 }
 
 func NewGResponse(raw http.ResponseWriter) (*GResponse){
 	out := new(GResponse)
 	out.raw = raw
+	out.charset = "utf-8"
 
 	return out
 }
@@ -32,4 +34,29 @@ func (resp *GResponse) Get(name string) (string){
 	return resp.RawWriter().Header().Get(name)
 }
 
+func (resp *GResponse) Cookie(cookie *http.Cookie) {
+	http.SetCookie(resp.RawWriter(), cookie)
+}
+
+func (resp *GResponse) Redirect(req *GRequest, urlStr string, code int) {
+	http.Redirect(resp.RawWriter(), req.RawRequest(), urlStr, code)
+}
+
+func (resp *GResponse) Location(urlStr string) {
+	resp.RawWriter().Header().Set("Location", urlStr)
+	http.Error(resp.RawWriter(), "Found", 302)
+}
+
+func (resp *GResponse) Charset(value string) {
+	resp.charset = value
+}
+
+func (resp *GResponse) Send(code int, body interface{}) {
+	if resp.RawWriter().Header().Get("Content-Type") == "" {
+		resp.RawWriter().Header().Set("Content-Type", "text/html; charset=" + resp.charset)
+	}
+
+	type := reflect.TypeOf(body)
+	
+}
 
